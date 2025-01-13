534204469476912e58385ee5a28fe28bd36efd30
Spark2009 <spark2009971@gmail.com>
2025-01-13 13:37:45 +0000

    spark

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ app.use(express.static(path.join(__dirname,"public")));
 app.set('view engine',"ejs")
 
 app.get('/',(req,res)=>{
-    res.send('ok ')
+    res.render('index');
+
 })
 
-app.get ('/:id',(req, res)=>{
-    res.render('index',{id:req.params.id});
+app.post('/create',(req, res)=>{
+    const name = req.body.name;
+    console.log('Name submitted:', name);
+    res.render('create', { name: name });
 });
 
 
 
-app.listen(3000,(err)=>{
-    console.log(err);
-})
+app.listen(3000, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+    } else {
+        console.log('Server is running on port 3000');
+    }
+});
