53e0fd93397bfdb5f1bf8aac8b5f19fb029c7f75
Spark2009 <spark2009971@gmail.com>
2025-01-13 11:59:53 +0000

    spark

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 app.set('view engine',"ejs")
+
+app.get('/',(req,res)=>{
+    res.send('ok ')
+})
+
 app.get ('/:id',(req, res)=>{
     res.render('index',{id:req.params.id});
 });
